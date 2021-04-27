f973722c2f541f94bf893f48d32a1bac36e3d151
weedskimhj <kimhj@weedscomm.com>
2021-04-27 16:07:39 +0900

    mongoose setting

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 
 // express app
 const app = express()
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:test1234@nodetuts.qf67u.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(5000))
+    .catch((err) => console.log(err))
 
 // register view engine
 app.set('view engine', 'ejs')
 
 // listen for requests
-app.listen(3000)
+// app.listen(3000)
 
-app.use((req, res, next) => {
-    console.log('new request made:')
-    console.log('host: ', req.hostname)
-    console.log('path: ', req.path)
-    console.log('method: ', req.method)
-    next()
-})
+// middleware & static files
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
 app.use((req, res, next) => {
     console.log('in the next middleware')
