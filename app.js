f2fbb37e9b42c9b72552b28ac265d4087bb45eae
weedskimhj <kimhj@weedscomm.com>
2021-04-27 10:29:40 +0900

    ejs 수업

--- app.js
+++ app.js
@@ -12,21 +12,34 @@ app.listen(3000)
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>')
     // absolute root
-    res.sendFile('./views/index.html', { root: __dirname })
+    // res.sendFile('./views/index.html', { root: __dirname })
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ]
+    res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>')
-    res.sendFile('./views/about.html', { root: __dirname })
+    // res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About' })
 })
 
 // redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about')
+// })
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' })
 })
+
 
 // 404 page
 // every request
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    // res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: '404' })
 })
