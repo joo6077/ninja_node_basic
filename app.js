83cc822f3dbd2b91ebe2ee90676de23b0fe1296a
weedskimhj <kimhj@weedscomm.com>
2021-04-27 17:49:40 +0900

    get post delete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const { render } = require('ejs')
 
 
 // express app
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs')
 
 // middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 
@@ -32,6 +34,40 @@ app.get('/blogs', (req, res) => {
             res.render('index', { title: 'All Blogs', blogs: result })
         })
         .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => {
             console.log(err);
         })
 })
